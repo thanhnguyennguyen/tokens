512b948ec58e01a00a44e6e76688fb0979c043bb
NguyenNguyen <nguyenbk92@gmail.com>
2021-08-10 12:39:51 +0700

    address toChecksum

--- generate.js
+++ generate.js
@@ -1,7 +1,23 @@
 const fs = require('fs')
 const path = require('path')
+const Web3 = require("web3")
+
+const TOMOCHAIN_RPC_ENDPOINT = `https://rpc.tomochain/com`
 
 const TOKEN_LIST_FILE = `tomochain.tokenlist.json`
+
+const timeout = 1000
+let web3
+try {
+    web3 = new Web3(new Web3.providers.HttpProvider(TOMOCHAIN_RPC_ENDPOINT, {
+        timeout: timeout
+    }))
+
+} catch (err) {
+    console.log(err)
+    process.exit(1)
+}
+
 const main = async () => {
     let tokens = {}
 
@@ -14,7 +30,7 @@ const main = async () => {
                 let fileName = `./tokens/${file}`
                 let token = require(fileName)
                 let s = fileName.split('/')
-                let tokenAddress = s[s.length - 1].split('.json')[0]
+                let tokenAddress = web3.utils.toChecksumAddress(s[s.length - 1].split('.json')[0])
                 token.logo = `https://raw.githubusercontent.com/thanhnguyennguyen/tokens/master/tokens/${tokenAddress}.png`
                 tokens[tokenAddress] = token
             } catch (error) {
